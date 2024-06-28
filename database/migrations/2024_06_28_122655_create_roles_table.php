8aebcffc36361caa5f1d51aab7e3899e1bcc7c42
Aris Papaprodromou <arispapapro@gmail.com>
2024-06-28 15:29:04 +0300

    Added Roles Database Migration.

---
+++ database/migrations/2024_06_28_122655_create_roles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+
+            // Auto Increment Integer
+            $table->id();
+
+            // Values
+            $table->string('label')->nullable();
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+
+            // Timestamps
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
+    }
+};
