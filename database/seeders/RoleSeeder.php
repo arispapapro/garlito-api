2dff0a948d501f12facf04b8715c11024cd2d829
Aris Papaprodromou <arispapapro@gmail.com>
2024-07-01 17:32:21 +0300

    [Added] Role Seeder.

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Configuration\GarlitoApiConfiguration;
+use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,15 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        // Get Default System Roles.
+        $default_api_roles = GarlitoApiConfiguration::get_default_api_roles();
+
+        // Create Role
+        foreach($default_api_roles as $role){
+
+            // Create Role.
+            Role::create($role);
+        }
     }
 }
