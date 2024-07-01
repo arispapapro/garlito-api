740d35dc181d97e62f8082510d1f17ceabab83de
Aris Papaprodromou <arispapapro@gmail.com>
2024-07-01 17:36:32 +0300

    [Added ] Basic CRUD of Role in Role Controller & Api Routing.

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// Utilities
+use AllowDynamicProperties;
+use App\Http\Resources\RoleResource;
+use Illuminate\Http\JsonResponse;
+
+// Helpers
+use App\Helpers\GarlitoModelHelper;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use OpenApi\Annotations as OA;
+use Illuminate\Http\Request;
+
+// Models
+use App\Models\Role;
+
+
+#[AllowDynamicProperties] class RoleController extends BaseApiController
+{
+
+
+
+
+    public function __construct()
+    {
+        //------------------------------------------------------------------------------------------------------------------
+        // Controller Configuration
+        //------------------------------------------------------------------------------------------------------------------
+
+        $this->model_single_name =  'role';
+        $this->model_plural_name =  'roles';
+        $this->option_label =  'slug';
+        $this->option_value =  'id';
+        $this->db_table =  'roles';
+        $this->default_resource =  RoleResource::class;
+        $this->model = Role::class;
+        $this->create_validation_rules = function(): array {
+            return [
+                'label' => ['string', 'required'],
+                'description' => ['string', 'required'],
+                'slug' => ['required', Rule::unique($this->db_table)]
+            ];
+        };
+        $this->update_validation_rules = function ($model = null ): array {
+            return [
+                'label' => ['string'],
+                'description' => ['string'],
+                'slug' => ['string', Rule::unique($this->db_table)->ignore($model)]
+            ];
+        };
+    }
+
+
+
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -6,15 +6,39 @@ use Illuminate\Support\Facades\Route;
 
 // Controllers Imported
 use App\Http\Controllers\ApiController;
+use App\Http\Controllers\RoleController;
 
-// Web Routing
-
-
+// Default Routes
 Route::get('/', [ApiController::class , 'default_api_page']);
 
 
+//----------------------------------------------------------------------------------------------------------------------
+// Model : Role
+//----------------------------------------------------------------------------------------------------------------------
 
+//======================================================================================================================
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+$model_single = 'role';
+$model_plural = 'roles';
+$model_controller = RoleController::class;
+//======================================================================================================================
+
+// Create
+Route::post('/'.$model_single, [$model_controller , 'create_model']);
+
+// Update Single
+Route::put('/'.$model_single.'/{id}', [$model_controller , 'update_model']);
+
+// Delete Single
+Route::delete('/'.$model_single.'/{id}', [$model_controller , 'delete_model']);
+
+// Fetch Single
+Route::get('/'.$model_single.'/{id}', [$model_controller , 'single_model']);
+
+// Fetch All
+Route::get('/'.$model_plural, [$model_controller , 'all_models']);
+
+// Fetch Options
+Route::get('/'.$model_single.'/options', [$model_controller , 'model_options']);
+
+//----------------------------------------------------------------------------------------------------------------------
